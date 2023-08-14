9a0e9e05de9d962d9ccf1f95f5bdd0df4989e3ca
nguyensythien <nguyensythien19979@gmail.com>
2023-08-15 02:30:36 +0700

    update evrything

--- js/main.js
+++ js/main.js
@@ -54,3 +54,29 @@ document.getElementById("switchButton").onclick = function () {
   //Sử dụng toggle() để thêm hoặc xóa 1 class của thẻ html. Nếu thẻ body không có class dark thì toggle sẽ thêm class dark vào body. Ngược lại, nếu thẻ body đang có class dark thì toogle sẽ xóa class dark đi.
 document.getElementById("myBody").classList.toggle("dark");
 };
+
+
+
+
+
+
+// BACK TO TOP
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
